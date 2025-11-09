ff25142ff784136c993478162a157b90ee1fece0
HimanshuSonwani <99082145+HimanshuSonwani@users.noreply.github.com>
2025-11-09 19:32:55 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
+  output: 'export',          // replaces "next export"
+  images: { unoptimized: true }, // needed if you use <Image/> with static export
+  trailingSlash: true        // optional; keeps urls consistent when served from /out
 };
 
 module.exports = nextConfig;
