e577f85093415075d83d3be35ef8f8ffe6432ff4
HimanshuSonwani <99082145+HimanshuSonwani@users.noreply.github.com>
2025-11-09 11:08:12 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone'
-}
-module.exports = nextConfig;
+// frontend/next.config.js
+module.exports = {
+  reactStrictMode: true,
+  output: 'standalone',
+};
