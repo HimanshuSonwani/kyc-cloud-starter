a1c6e0eef148a99f09a4ffdfcac7cc770c03ef68
HimanshuSonwani <99082145+HimanshuSonwani@users.noreply.github.com>
2025-11-09 12:07:32 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // produce the .next/standalone server.js that we run on Railway
+  // this tells Next to produce .next/standalone
   output: 'standalone'
 };
-
 module.exports = nextConfig;
