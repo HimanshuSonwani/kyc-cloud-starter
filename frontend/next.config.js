a22e912d48b46a5d64150c9defac73bd03b06849
HimanshuSonwani <99082145+HimanshuSonwani@users.noreply.github.com>
2025-11-09 20:08:58 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,6 @@
+// frontend/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // we are serving as static; export is configured here (do not run `next export`)
   output: 'export',
-  images: { unoptimized: true },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://kyc-cloud-starter-production.up.railway.app/:path*',
-      },
-    ];
-  },
 };
 module.exports = nextConfig;
