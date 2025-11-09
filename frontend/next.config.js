5078dc315cd6b90879592e364dbf94080f29fd2b
HimanshuSonwani <99082145+HimanshuSonwani@users.noreply.github.com>
2025-11-09 12:33:19 +0530

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,7 @@
+// /frontend/next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // IMPORTANT: do NOT set output: 'standalone' here
+  reactStrictMode: true,
+};
+module.exports = nextConfig;
