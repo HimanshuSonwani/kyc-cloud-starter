e288d4cd9aeb962a5e5bec602f02940fdd93f57d
HimanshuSonwani <99082145+HimanshuSonwani@users.noreply.github.com>
2025-11-09 23:07:11 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',             // replaces `next export`
-  images: { unoptimized: true } // optional: avoids Image Optimization on static hosts
+  reactStrictMode: true
 };
 module.exports = nextConfig;
